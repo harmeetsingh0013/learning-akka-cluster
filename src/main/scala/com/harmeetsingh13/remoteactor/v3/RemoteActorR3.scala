736f08d921ad412e6912814a0453ea3b6737fd6e
harmeetsingh0013 <harmeetsingh.0013@gmail.com>
2017-03-03 13:22:22 +0530

    remote actor programmatically done successfully

--- src/main/scala/com/harmeetsingh13/remoteactor/v3/RemoteActorR3.scala
+++ src/main/scala/com/harmeetsingh13/remoteactor/v3/RemoteActorR3.scala
@@ -2,6 +2,7 @@ package com.harmeetsingh13.remoteactor.v3
 
 import akka.actor.{ActorSystem, AddressFromURIString, Deploy, Props}
 import akka.remote.RemoteScope
+import com.typesafe.config.ConfigFactory
 
 /**
   * Created by harmeet on 3/3/17.
@@ -16,10 +17,30 @@ object RemoteActorR3 extends App {
     Deploy(scope = RemoteScope(remoteR1Address))
   )
 
-  val ref = ActorSystem("remote-r3")
+  val conf =
+    """
+      |akka {
+      |  actor {
+      |    provider = "akka.remote.RemoteActorRefProvider"
+      |  }
+      |
+      |  remote {
+      |    enabled-transports = ["akka.remote.netty.tcp"]
+      |    netty.tcp {
+      |      hostname = "0.0.0.0"
+      |      port = 2553
+      |    }
+      |  }
+      |}
+    """.stripMargin
+
+  val config = ConfigFactory.parseString(conf)
+  val ref = ActorSystem("remote-r3", config)
   val remoteR1 = ref.actorOf(props, "echo")
 
   remoteR1 ! "Hello Dude1"
   remoteR1 ! "Hello Dude2"
   remoteR1 ! "Hello Dude3"
+
+
 }
